bb468e6e63aee59e5721ac02b2068e81f47fa607
Jose Delgado <pianodude10@yahoo.com>
2017-01-13 22:26:00 -0800

    Added Important Symbol

---
+++ DoIt/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Jose Delgado on 1/13/17.
+//  Copyright © 2017 Jose Delgado. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
