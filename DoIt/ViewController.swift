d36a33b91595db1453055d8bb908dea9d5d66415
Jose Delgado <pianodude10@yahoo.com>
2017-01-13 19:43:10 -0800

    Initial Commit

---
+++ DoIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DoIt
+//
+//  Created by Jose Delgado on 1/13/17.
+//  Copyright © 2017 Jose Delgado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
