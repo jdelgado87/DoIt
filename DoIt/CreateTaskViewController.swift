3b78381017203c1a06d396af58fb205a4d753518
Jose Delgado <pianodude10@yahoo.com>
2017-01-16 21:34:09 -0800

    Added Task List View Controller

---
+++ DoIt/CreateTaskViewController.swift
@@ -0,0 +1,26 @@
+//
+//  CreateTaskViewController.swift
+//  DoIt
+//
+//  Created by Jose Delgado on 1/16/17.
+//  Copyright © 2017 Jose Delgado. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    @IBOutlet weak var taskNameTextField: UITextField!
+    
+    @IBOutlet weak var importantSwitch: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func addTapped(_ sender: Any) {
+    }
+
+ 
+}
